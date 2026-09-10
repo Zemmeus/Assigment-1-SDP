e71f0b3bd56a057122fcd997df3bbf04451373a6
RamilSirius <ramil@thesirius.ai>
2026-09-10 19:31:51 +0500

    add character model with items and modifiers

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Character;
+import model.EquipmentSlot;
+import model.Item;
+import model.ItemModifier;
+import model.StatType;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Item sword = new Item(
+                "Sword of Dawn",
+                EquipmentSlot.MAIN_HAND,
+                List.of(
+                        new ItemModifier(StatType.STRENGTH, 5),
+                        new ItemModifier(StatType.CRIT_CHANCE, 3)
+                )
+        );
 
+        Item ring = new Item(
+                "Ring of Shadows",
+                EquipmentSlot.FINGER,
+                List.of(
+                        new ItemModifier(StatType.SHADOW_RESISTANCE, 12),
+                        new ItemModifier(StatType.INTELLECT, -2)
+                )
+        );
+
+        Character warrior = new Character(
+                "Ragnar",
+                "Orc",
+                "Warrior",
+                10,
+                25,
+                8,
+                5,
+                List.of(sword, ring)
+        );
+
+        System.out.println(warrior);
     }
 }
